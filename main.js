b2002cb429ea87ae81247f4982cdaf88f93c4070
Émilie Fontaine <emilie@fontaine.net>
2021-11-09 23:24:42 -0500

    lemme try something

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("Hello world!");
+
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  effect: 'cards',
+
+ 
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  });  
+
